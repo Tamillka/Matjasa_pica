02af03b6f6062e0547f27655dda078eda0ebcbb1
Tamila <Tamila.matjasha#gmail.com>
2023-03-27 21:14:22 +0300

    Iesācu metodi, kura reģistrē klientu un viņa personālos datus, ja izvelēta piegāde

--- src/Klients.java
+++ src/Klients.java
@@ -1,16 +1,16 @@
 public class Klients {
-		String vards, uzvards, adrese,talrunis;
+		String vards, adrese,talrunis;
 		boolean piegade;
+		
 
-		public Klients(String vards, String uzvards, String adrese, String talrunis,boolean piegade) {
+		public Klients(String vards, String adrese, String talrunis,boolean piegade) {
 			this.vards = vards;
-			this.uzvards = uzvards;
 			this.adrese = adrese;
 			this.talrunis = talrunis;
 			this.piegade = piegade;
 		}
 
 		public String izvadit() {
-			return "\n------------------------------\nVārds: "+vards+" "+uzvards+"\nTālrunis: "+talrunis+"\nPiegādes adrese: "+adrese;
+			return "\n------------------------------\nVārds: "+vards+"\nTālrunis: "+talrunis+"\nPiegādes adrese: "+adrese;
 		}
 }
--- src/Picerija.java
+++ src/Picerija.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 public class Picerija {
-
+	static ArrayList<Klients> klienti = new ArrayList<>();
+	
+	public static void izveidotKlientu() {
+	int gNr=0;
+		String vards="", talrunis="", adrese="";	
+		boolean piegade = false;
+		int pieg = (JOptionPane.showConfirmDialog(null, "Būs piegāde mājās?", "Piegāde mājās", JOptionPane.YES_NO_OPTION));
+		
+		if (pieg==JOptionPane.YES_OPTION){	
+			 vards += (String)JOptionPane.showInputDialog("Vārds");
+			 talrunis += (String)JOptionPane.showInputDialog("Tālrunis");
+	  adrese = (String)JOptionPane.showInputDialog("Adrese");	
+	 piegade = true;
+		}else if(pieg==JOptionPane.NO_OPTION) {
+		    vards += "-";
+		    talrunis += "-";
+			adrese += "-";
+			
+		}  		
+		Klients konts = new Klients(vards, adrese, talrunis, piegade);	
+		klienti.add(konts);
+	}
 	public static void main(String[] args) {
 		
 
